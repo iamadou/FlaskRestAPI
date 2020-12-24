b33bd6ad40b75d76e006b276c4c798ec0c28f9fd
Ibrahim Amadou <amadou68@gmail.com>
2020-12-24 01:46:19 +0100

    MongoDB final version

--- movie-bag/app.py
+++ movie-bag/app.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 
-from flask import Flask, jsonify, request
+from flask import Flask, request, Response
+from database.db import initialize_db
+from database.models import Movie
+import json
+
 
 
 app = Flask(__name__)
 
+"""
 movies = [
     {
         "name": "The Shawshank Redemption",
@@ -19,38 +24,48 @@ movies = [
        "genres": ["Crime", "Drama"]
     }
 ]
+"""
 
-@app.route('/movies', methods=['GET'])
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://localhost/movie-bag'
+}
+
+initialize_db(app)
+
+
+@app.route('/movies')
 def get_movies():
-    return jsonify(movies)
-    
-    
+    movies = Movie.objects().to_json()
+    return Response(movies, mimetype="application/json", status=200)   
+
+
+@app.route('/movies/<id>')
+def get_movie(id):
+    movie = Movie.objects.get(id=id).to_json()
+    return Response(movie, mimetype="application/json", status=200)   
+
+
 @app.route('/movies', methods=['POST'])
 def add_movies():
-
-    movie = request.get_json()
-    print(movie)
-    movies.append(movie)
-    print(movies)
-    return {'id': len(movies)}, 200
+    body = request.get_json()
+    movie = Movie(**body).save()
+    id = movie.id
+    return {'id': str(id)}, 200
 
 
-@app.route('/movies/<int:index>', methods=['PUT'])
-def update_movie(index):
-    print(index)
-    movie = request.get_json()
-    print(request)
-    print(movie)
-    print(movies[index])
-    movies[index] = movie
-    print(movies[index])
-    return jsonify(movies[index]), 200
+
+@app.route('/movies/<id>', methods=['PUT'])
+def update_movie(id):
+    body = request.get_json()
+    Movie.objects.get(id=id).update(**body)
+    return '', 200
     
 
-@app.route('/movies/<int:index>', methods=['DELETE'])
-def delete_movie(index):
-    movies.pop(index)
-    return 'None', 200
+
+@app.route('/movies/<id>', methods=['DELETE'])
+def delete_movie(id):
+    Movie.objects.get(id=id).delete()
+    return '', 200
 
 
 
