8f6ab5d653d2c6c548e94bcf709be498dc18dfec
Ibrahim Amadou <amadou68@gmail.com>
2020-12-27 15:25:33 +0100

    update

--- movie-bag/app.py
+++ movie-bag/app.py
@@ -2,18 +2,21 @@
 
 
 from flask import Flask
+from flask_restful import Api
 from database.db import initialize_db
-from resources.movie import movies
+from resources.routes import initialize_routes
+
 
 
 app = Flask(__name__)
+api = Api(app)
 
 
 app.config["MONGODB_SETTINGS"] = {"host": "mongodb://localhost/movie-bag"}
 
 initialize_db(app)
-app.register_blueprint(movies)
 
+initialize_routes(api)
 
 if __name__ == "__main__":
     app.run()
