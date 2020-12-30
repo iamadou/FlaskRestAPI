99f7b6fb8bdf1b4dee47cc702d9589b0527e42b5
Ibrahim Amadou <amadou68@gmail.com>
2020-12-30 21:36:27 +0100

    test login process

---
+++ movie-bag/tests/test_signup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+import json
+from tests.BaseCase import BaseCase
+
+
+
+class SignupTest(BaseCase):
+
+        
+    def test_successful_signup(self):
+        # Given
+        payload = json.dumps({
+                "name": "paura Kh",
+        	"email": "paurakh011@gmail.com",
+        	"password": "mycoolpassword"
+        })
+        
+        # When
+        response = self.app.post('/api/auth/signup', headers={'Content-Type': "application/json"}, data=payload)
+        
+        # Then
+        self.assertEqual(str, type(response.json['id']))
+        self.assertEqual(200, response.status_code)
+        
